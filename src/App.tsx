f12084547dac4728a0fe8e29aa60ea5fcc9466ab
Dillon Cutaiar <dillonc@vt.edu>
2022-05-08 22:33:41 -0400

    Remove some CRA stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Going to be making Morris</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Cutaiar/morris"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more
         </a>
       </header>
     </div>

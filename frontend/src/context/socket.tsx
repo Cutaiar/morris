1fb137ecdfac368e5fb2c302747f3f3d3576628b
Dillon Cutaiar <dillonc@vt.edu>
2022-07-01 00:57:33 -0600

    revert socket uri

--- frontend/src/context/socket.tsx
+++ frontend/src/context/socket.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { io, Socket } from "socket.io-client";
 
 // TODO: switch between dev and prod dynamically
-const uri = "http://localhost:1337";
-// const uri = "https://morris-socket-server.herokuapp.com";
+// const uri = "http://localhost:1337";
+const uri = "https://morris-socket-server.herokuapp.com";
 
 interface SocketContextValue {
   socket?: Socket;

0af3d73d8b7e3a2494e1b5e7bd4706a7bbe8b628
Dillon Cutaiar <dillonc@vt.edu>
2022-09-08 21:37:25 -0600

    Use initial state 9 rather than generation

--- frontend/src/hooks/useGameState.ts
+++ frontend/src/hooks/useGameState.ts
@@ -1,12 +1,13 @@
 import React from "react";
 import { reducer } from "morris-core";
-import { generate } from "morris-core/initialState";
+import { generate, initialStateNine } from "morris-core/initialState";
 
 /**
  * A reducer-like hook which holds the entire state of the game
  */
 export const useGameState = () => {
-  return React.useReducer(reducer, 2, generate);
+  return React.useReducer(reducer, initialStateNine);
+  // return React.useReducer(reducer, 3, generate);
 };
 
 // For back compat with existing imports in the front end

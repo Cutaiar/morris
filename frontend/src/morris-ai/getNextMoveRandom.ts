b9bcd825ddbbf1368a8bea0fb3ab97ec2bf800d1
Dillon Cutaiar <dillonc@vt.edu>
2023-02-27 23:29:40 -0700

    more efficient new implementation of getMoveRandom

--- frontend/src/morris-ai/getNextMoveRandom.ts
+++ frontend/src/morris-ai/getNextMoveRandom.ts
@@ -1,12 +1,14 @@
-import { Action, GameState, isValidAction } from "morris-core";
-import { getRandomProperty } from "morris-core/utils";
+import { Action, GameState, isValidAction, nextValidMoves } from "morris-core";
+import { getRandomElement, getRandomProperty } from "morris-core/utils";
 import { NextMoveFunction } from "./types";
 
 /**
  * Get the next move an opponent should make given the status to the game.
  * Moves are picked at random
  */
-export const getNextMoveRandom: NextMoveFunction = (state: GameState) => {
+export const getNextMoveRandomRecursive: NextMoveFunction = (
+  state: GameState
+) => {
   // Generate an action at random, but appropriate to what is happening in the game
   let action: Action | undefined = undefined;
   if (state.turn.type === "remove") {
@@ -35,5 +37,32 @@ export const getNextMoveRandom: NextMoveFunction = (state: GameState) => {
   }
 
   // Found a valid move so return it for dispatch
+  return action;
+};
+
+/** A better implementation of the fn with the same name above */
+export const getNextMoveRandom: NextMoveFunction = (gameState: GameState) => {
+  const moves = nextValidMoves(gameState);
+
+  let action: Action;
+  if (Array.isArray(moves)) {
+    const to = getRandomElement(moves);
+    action = {
+      type: gameState.turn.type === "remove" ? "remove" : "place",
+      to,
+    };
+  } else {
+    const nonEmptyMoves = Object.entries(moves)
+      .filter((m) => m[1].length !== 0)
+      .map((m) => m[0]);
+    const from = getRandomElement(nonEmptyMoves);
+    const to = getRandomElement(moves[from]);
+    action = { type: "move", from, to };
+  }
+
+  if (!action) {
+    throw new Error("getNextMoveRandom was unable to produce a valid action");
+  }
+
   return action;
 };

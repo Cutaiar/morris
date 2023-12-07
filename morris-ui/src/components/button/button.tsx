ad27637cc89140dc42c80bd41ae0959d772207d6
Dillon Cutaiar <dmcutaiar@gmail.com>
2023-12-06 20:03:47 -0700

    button uses sc

--- morris-ui/src/components/button/button.tsx
+++ morris-ui/src/components/button/button.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-
-// Style
-import { fontSizes, palette } from "theme";
-import "./button.css";
+import styled from "styled-components";
 
 // Components
 import { Loader } from "components";
+
 export interface ButtonProps
   extends React.PropsWithChildren<{
     onClick?: () => void;
@@ -15,58 +13,59 @@ export interface ButtonProps
   }> {}
 
 export const Button = (props: ButtonProps) => {
-  const { onClick, disabled, primary, loading } = props;
-
-  // TODO: Better support for disabled
-  const bgColor = primary
-    ? palette.primary
-    : palette.surface;
-
-  const textColor = primary ? palette.surface : palette.neutralDark;
-
-  const disabledStyle: React.CSSProperties = {
-    borderColor: palette.neutralLighter,
-    color: palette.neutralLighter,
-    transform: "none",
-    boxShadow: "none",
-    cursor: "not-allowed"
-  };
-
-  const extraStyle = {
-    ...(disabled ? disabledStyle : {}),
-  };
+  const { onClick, disabled, primary, loading, children } = props;
 
   return (
-    <button
+    <Root
       disabled={disabled}
-      className={"button-component"}
-      style={{
-        minWidth: 60,
-        minHeight: 30,
-        backgroundColor: bgColor,
-        color: textColor,
-        borderRadius: 4,
-        borderStyle: "solid",
-        padding: 8,
-        borderWidth: 1,
-        borderColor: textColor,
-        fontSize: fontSizes.medium,
-        fontFamily: "inherit",
-        ...extraStyle
-      }}
       onClick={onClick}
+      primary={primary}
     >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: 8,
-        }}
-      >
-        {props.children}
+      <Content>
+        {children}
         {loading && <Loader />}
-      </div>
-    </button>
+      </Content>
+    </Root>
   );
 };
+
+
+const Content = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8;
+`;
+
+const Root = styled.button<{ primary?: boolean }>`
+  transition: transform 0.2s ease;
+  transition: box-shadow 0.1s ease;
+  cursor: pointer;
+
+  min-width: 60px;
+  min-height: 30px;
+  background-color: ${({ primary, theme }) =>
+    primary ? theme.palette.primary : theme.palette.surface};
+  color: ${({ primary, theme }) =>
+    primary ? theme.palette.surface : theme.palette.neutralDark};
+  border-radius: 4px;
+  border-style: solid;
+  padding: 8px;
+  border-width: 1px;
+  border-color: currentColor;
+  font-size: var(--morris-font-size-medium);
+  font-family: inherit;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: var(--shadow-elevation-medium);
+  }
+
+  &:disabled {
+    border-color: ${({theme}) => theme.palette.neutralLighter};
+    color: ${({theme}) => theme.palette.neutralLighter};
+    transform: none;
+    box-shadow: none;
+    cursor: not-allowed;
+  }
+`;

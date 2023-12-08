4618c22373ffe325aa9da9b3404bdae3e3ddc320
Dillon Cutaiar <dmcutaiar@gmail.com>
2023-12-07 20:51:26 -0700

    loader uses sc

--- morris-ui/src/components/loader/loader.tsx
+++ morris-ui/src/components/loader/loader.tsx
@@ -1,31 +1,53 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-//Style
-import { fontSizes, palette } from "theme";
-import "./loader.css";
-
-export interface LoaderProps
-  extends Pick<React.HTMLAttributes<HTMLDivElement>, "style" | "className"> {
+export interface LoaderProps {
   text?: string;
 }
 
 export const Loader = (props: LoaderProps) => {
-  const { className, text } = props;
-  const style: React.CSSProperties = {
-    height: 20,
-    display: "flex",
-    alignItems: "center",
-    gap: 8,
-    ...props.style,
-  };
+  const { text } = props;
+
   return (
-    <div style={style} className={className}>
-      <div className="loading" />
+    <Root>
+      <Spinner />
       {text && (
-        <span style={{ color: palette.neutral, fontSize: fontSizes.medium }}>
+        <Text>
           {text}
-        </span>
+        </Text>
       )}
-    </div>
+    </Root>
   );
 };
+
+const Root = styled.div`
+    height: 20px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+`;
+
+const spin = keyframes`
+  to {
+    transform: rotate(1turn);
+  }
+`;
+
+const Spinner = styled.div`
+  display: flex;
+  justify-content: center;
+  &::after {
+    content: "";
+    width: 15px;
+    height: 15px;
+    border: 2px solid transparent;
+    border-top-color: var(--morris-palette-neutral);
+    border-radius: 50%;
+    animation: ${spin} 1s ease infinite;
+  }
+`;
+
+const Text = styled.span`
+  color: ${({theme}) => theme.palette.neutral};
+  font-size: ${({theme}) => theme.fontSizes.medium};
+`;

a88db3bb5b241d91299dfeebb318e22603872dae
Dillon Cutaiar <dmcutaiar@gmail.com>
2023-12-07 21:27:32 -0700

    toRemove uses sc

--- morris-ui/src/components/playerCard/ToRemove.tsx
+++ morris-ui/src/components/playerCard/ToRemove.tsx
@@ -1,25 +1,23 @@
 import * as React from "react";
-import { fontSizes, palette } from "theme";
+import styled from "styled-components";
 
 /**
  * A tag to be shown in indication that the current turn is a removal
  */
 export const ToRemove = () => {
   return (
-    <div
-      style={{
-        background: palette.surface,
-        borderRadius: 5,
-        padding: 5,
-        boxSizing: "border-box",
-        fontSize: fontSizes.small,
-        color: palette.neutral,
-        borderColor: palette.neutral,
-        borderStyle: "solid",
-        borderWidth: 1,
-      }}
-    >
-      {"to remove"}
-    </div>
+    <Root>to remove</Root>
   );
 };
+
+const Root = styled.div`
+  background: ${({theme})=> theme.palette.surface};
+  border-radius: 5px;
+  padding: 5px;
+  box-sizing: border-box;
+  font-size: ${({theme})=> theme.fontSizes.small};
+  color: ${({theme})=> theme.palette.neutral};
+  border-color: ${({theme})=> theme.palette.neutral};
+  border-style: solid;
+  border-width: 1px;
+`;

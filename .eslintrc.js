99f0b7a4edf4b777be0372ddf5ef820ae67e8981
Dillon Cutaiar <dmcutaiar@gmail.com>
2023-12-08 21:20:23 -0700

    linting and formatting working

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: ["./*/tsconfig.json"]
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {}
+};
